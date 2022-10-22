3920f1c35e1e5479191bdcc3f242465386ba95be
estabravo <josuebravo.dev@gmail.com>
2022-10-22 02:50:59 -0700

    added toggle for skills

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -47,4 +47,42 @@
 
           window.addEventListener("scroll", reveal);
 
-         
+
+          const btn = document.querySelector("#button-18");
+          const para = document.querySelectorAll('.iconToggle');
+          
+          btn.addEventListener('click',()=>{
+            para.forEach(el => {
+              el.classList.toggle('d-none');
+            })
+          })
+
+
+          //toggle var
+// var fade_state = true;
+// //on btn click
+// function fade() {
+//   //get the button and div
+//   let div = document.getElementById("div");
+//   let btn = document.getElementById("fade");
+//   //if faded in or out
+//   if (fade_state == true) {
+//     //trigers animation
+//     div.style.animation = "fade-out 2s forwards";
+//     //sets the text
+//     btn.innerHTML = "fade-in";
+//     //sets fade_state
+//     fade_state = false;
+//   } else if (fade_state == false) {
+//     //trigers animation
+//     div.style.animation = "fade-in 2s forwards";
+//     //sets the text
+//     btn.innerHTML = "fade-out";
+//     //sets fade_state
+//     fade_state = true;
+//   }
+// }
+
+window.onload = function(){
+  clearItForm.reset();
+}
