85ba6e01520567f1960ecbe2e99ef3b33e67c03a
estabravo <josuebravo.dev@gmail.com>
2022-10-27 22:04:46 -0700

    changed fade interaction time

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -34,7 +34,7 @@
             for (let i = 0; i < reveals.length; i++) {
               const windowHeight = window.innerHeight;
               const elementTop = reveals[i].getBoundingClientRect().top;
-              const elementVisible = 150;
+              const elementVisible = 110;
           
               if (elementTop < windowHeight - elementVisible) {
                 reveals[i].classList.add("active");
